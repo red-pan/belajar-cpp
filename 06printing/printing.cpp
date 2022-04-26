a42955b01cf4cc300226122ebb141eac175a8f4d
red-pan <redpandev@gmail.com>
2022-04-27 06:20:12 +0700

    add source code

---
+++ 06printing/printing.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+// this is a single line comment
+/*
+this
+is
+a
+multi
+line
+comment
+*/
+
+int main (){
+    std::cout << "Hello, World\n";
+    std::cout << "This a new line sentence" << std::endl;
+    return 0;
+}
