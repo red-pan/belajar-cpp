9c8144ecb98a7048f2e409759373c18004e589b9
red-pan <redpandev@gmail.com>
2022-04-27 23:48:41 +0700

    deklarasi

---
+++ 08deklarasi/deklarasi.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std; //tidak disarankan
+
+int main () {
+    //deklarasi
+    int a;
+    //assignment
+    a = 10;
+    cout << "nilai a: " << a << "\n";
+
+    int b;
+    cout << "masukkan nilai integer: ";
+    cin >> b;
+    cout << "nilai b: " << b << "\n";
+
+    return 0;
+}
