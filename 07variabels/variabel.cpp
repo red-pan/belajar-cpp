6987c56f6ad00a77456447d145998ff352e0fba9
red-pan <redpandev@gmail.com>
2022-04-27 16:56:17 +0700

    menambahkan folder dan file variabel

---
+++ 07variabels/variabel.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    int variabelInteger;
+
+    variabelInteger = 27;
+
+    cout << "Nilai variabel: " << variabelInteger << endl;
+
+    return 0;
+}
